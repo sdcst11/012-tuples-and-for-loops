7657034dd9987813f58a30955e25eac04e5e9ec0
Chris Yang <cyang@deltalearns.ca>
2023-03-07 09:18:45 -0800

    update for 2023

--- example1.py
+++ example1.py
@@ -1,19 +1,33 @@
 #! python3
 
 """
-A tuple is a variable type that contains multiple pieces of data. To access the members of a tuple,
-you can use a for x in y: loop or try to call the individual members using an "index"
-An index is an integer in a set of [] following the tuple name.  The numbers in the index
-start at 0 and increase as integers
+A tuple or list is a variable type that contains multiple pieces of data. To access the members of a tuple or list, you can use a for x in y: loop or try to call the individual members using an "index" An index is an integer in a set of [] following the tuple name.  The numbers in the index start at 0 and increase as integers
 """
 print("===================")
-print("     tuples        ")
+print(" tuples and lists  ")
 print("===================")
 print("\n\n")
 # this is how a tuple is defined
 myFriends = ("Carol","Christine","Dan","Richard")
 
-# to reference members of the tuple, use an index:
+# this is how a list is defined
+otherFriends = ["Oscar","Benjamin","Casey","Chessy"]
+
+# we can print out the entire list or tuple using its name
+print(myFriends)
+print(otherFriends)
+
+
+# to reference members of the tuple or list, use an index inside the square bracket:
 # the index starts at 0 and increases by 1
 print(myFriends[1] + " is one of the people that I have known the longest!")
-print("\n\n\n")
+print(f"{otherFriends[0]} is a good friend!")
+print("\n")
+# we can easily change the value of a list item:
+otherFriends[2] = "Chessy"
+print(otherFriends)
+
+print("\n")
+# if we try to change the value of a tuple item, we get an error:
+# TypeError: 'tuple' object does not support item assignment
+myFriends[3] = "Ricky"
--- example2d.py
+++ example2d.py
@@ -1,6 +1,9 @@
 """
 If the for loop runs through the entire list of values,
 it will automatically run the else statement as well
+range(10) actually represents a tuple that looks like
+(0,1,2,3,4,5,6,7,8,9)
+and then iterates through the list
 """
 
 for i in range(10):
